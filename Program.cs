ac530a3ac634378752bd6f71ecf0697dbd9776a3
Angelov0Angel <angelov0angel@gmail.com>
2022-10-20 18:56:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+{
+    string[] names = { "Alonso", "Zheng", "Smith", "Jones", "Smythe", "Small", "Ruiz",
+        "Hsieh", "Jorgenson", "Ilyich", "Singh", "Samba", "Fatimah" };
+    var queryResults =
+        from n in names
+        where n.StartsWith("S")
+        select n;
+    WriteLine("Names beginning with S:");
+    foreach (var item in queryResults)
+    {
+        WriteLine(Item);
+    }
+}   Write("Program finished, press Enter/Return to continue");
